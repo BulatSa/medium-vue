3e4f74e0162710e289af8a4fdf346e9caba3cf83
Bulat <sadikovba@gmail.com>
2020-12-09 23:24:02 +0300

    Maked pagination

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ export default {
     McvTopbar
   },
   mounted() {
-    console.log('mounted');
     this.$store.dispatch(actionTypes.getCurrentUser)
   }
 };
--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -43,10 +43,10 @@
         </router-link>
       </div>
       <mcv-pagination
-        :total="total"
+        :total="feed.articlesCount"
         :limit="limit"
         :current-page="currentPage"
-        :url="url"
+        :url="baseUrl"
       />
     </div>
   </div>
@@ -56,6 +56,9 @@
 import {mapState} from 'vuex';
 import {actionTypes} from '@/store/modules/feed';
 import McvPagination from '@/components/Pagination';
+import {limit} from '@/helpers/vars';
+import {stringify, parseUrl} from 'query-string';
+
 export default {
   name: 'McvFeed',
   props: {
@@ -66,9 +69,7 @@ export default {
   },
   data() {
     return {
-      total: 500,
-      limit: 10,
-      currentPage: 5,
+      limit,
       url: '/tags/dragons'
     };
   },
@@ -80,10 +81,38 @@ export default {
       isLoading: state => state.feed.isLoading,
       feed: state => state.feed.data,
       error: state => state.feed.error
-    })
+    }),
+    currentPage() {
+      return Number(this.$route.query.page || '1');
+    },
+    baseUrl() {
+      return this.$route.path;
+    },
+    offset() {
+      return this.currentPage * limit - limit;
+    }
+  },
+  watch: {
+    currentPage() {
+      console.log('change page');
+      this.fetchFeed();
+    }
   },
   mounted() {
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl});
+    this.fetchFeed();
+  },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl);
+      const stringifiedParams = stringify({
+        limit,
+        offset: this.offset,
+        ...parsedUrl.query
+      });
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+      console.log(apiUrlWithParams)
+      this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams});
+    }
   }
 };
 </script>
